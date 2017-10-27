30cb3695a04af440d8e6ce1efe54eaddbfdb9591
Faza Mahamood <fazamhd@gmail.com>
2017-10-27 11:46:01 +0530

    Improved the stub for R
    Fixes #7
    
    Signed-off-by: Faza Mahamood <fazamhd@gmail.com>

--- stub.r
+++ stub.r
@@ -1,1 +1,10 @@
-cat("Hello world\n")
+f <- file("stdin")
+on.exit(close(f))
+
+# Read the variable from STDIN
+lines <- readLines(f)
+num <- as.numeric(lines[[1]])
+
+# Write the variable to STDOUT
+write(num, stdout())
+
